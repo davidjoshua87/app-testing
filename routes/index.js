57021c70bd2951e1b9a737c9288dfcfaaac01b14
David Joshua <davidjosh_87@yahoo.com>
2018-07-02 07:17:34 +0700

    added file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const index  = require('../controllers/index_controller')
 
+//route index
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 //route login
 router.post('/login', index.loginUser);
 
